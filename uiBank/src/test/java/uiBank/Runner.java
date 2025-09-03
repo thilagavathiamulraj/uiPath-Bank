7959c44ec4e9badb641f9f195aec36d18880e175
Thilagavathi Amulraj <131727663+thilagavathiamulraj@users.noreply.github.com>
2025-09-03 13:04:59 -0500

    Update Runner.java
    Updated Tests

--- uiBank/src/test/java/uiBank/Runner.java
+++ uiBank/src/test/java/uiBank/Runner.java
@@ -4,29 +4,42 @@ import org.testng.annotations.Test;
 
 public class Runner {
 
-	@Test
+	@Test(priority=1)
 	void login() {
-		
+
 		Login login = new Login();
-		
+
 		login.launch();
-		
+
 		login.login();
-		
-		ApplyAccount applyAccount = new ApplyAccount();
-		
-		applyAccount.applyforAccount();
-		
-		FundTransfer transfer = new FundTransfer();
-		
-		transfer.transferFund();
-		
-		ApplyLoan applyLoan =new ApplyLoan();
-		
-		applyLoan.applyForLoan();
-		
-		applyLoan.retriverLoanDetails();
-		
+
 	}
-	
+	@Test(priority=2)
+	void ApplyForAnAccount() {
+
+		ApplyAccount applyAccount = new ApplyAccount();
+
+		applyAccount.applyforAccount();
+
+	}
+
+	@Test(priority=3)
+	void fundTransferToAnotherAccount() {
+
+		FundTransfer transfer = new FundTransfer();
+
+		transfer.transferFund();
+	}
+
+	@Test(priority=4)
+	void applyForALoan() throws Exception {
+
+		ApplyLoan applyLoan = new ApplyLoan();
+
+		applyLoan.applyForLoan();
+
+		applyLoan.retriverLoanDetails();
+
+	}
+
 }
